29437eefd48bbde3e92222428f7b787ea6e1a1d2
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-04-27 11:25:08 +0500

    Mini Assignment - 03

--- app.js
+++ app.js
@@ -30,3 +30,20 @@ for (i = 0; i < arr1.length; i++) {
         console.log(arr1[i][j]+ arr2[i][j]);
     }
 }
+
+
+// Mini Assignment - 03
+
+// let array1 = [[1,2,7], [2,5], [9, 0, 2]];
+// let array2 = [[1,6,3], [4,5], [8, 4, 4]];
+
+// Square of array1
+// Result = [2, 8, 10, 6, 10, 17, 4, 6]
+
+
+let array1 = [[1,2,7], [2,5], [9, 0, 2]];
+for (i = 0; i < array1.length; i++) {
+    for (j = 0; j < array1[i].length; j++) {
+        console.log(array1[i][j] * array1[i][j]);
+    }
+}
