ad6488f0745b233eee0729edc06f2be934c99254
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-04-28 15:09:14 +0500

    Mini Assignment - 01

---
+++ index1.js
@@ -0,0 +1,13 @@
+// Mini Assignment - 01
+
+// let arr = [[1,2,3] , [4,5]];
+// Result = [1,2,3,4,5]
+
+
+let arr = [[1,2,3] , [4,5]];
+
+for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j]);
+    }
+}
